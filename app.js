1d838185cc5f0deefb20b9e3e6ba3b32edd989fe
Jack <jack.branthwaite@gmail.com>
2021-02-16 21:14:14 +1300

    hands now change on click, and score updates

--- app.js
+++ app.js
@@ -21,10 +21,84 @@ const game = () => {
     const computerHand = document.querySelector(".computer-hand");
     //Computer's options
     const computerOptions = ["rock", "paper", "scissors"];
+    const computerNumber = Math.floor(Math.random() * 3);
+
+    //when using this and option we need to make sure that we use a function not arrow function
+    options.forEach((option) => {
+      option.addEventListener("click", function () {
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+        //Call compare hands
+        compareHands(this.textContent, computerChoice);
+        //Update images
+        playerHand.src = `./assets/${this.textContent}.png`;
+        computerHand.src = `./assets/${computerChoice}.png`;
+      });
+    });
+  };
+
+  const compareHands = (playerChoice, computerChoice) => {
+    //Update text
+    const winner = document.querySelector(".winner");
+    //Check for a draw
+    if (playerChoice === computerChoice) {
+      winner.textContent = "It is a tie";
+      return;
+    }
+    //Check for rock
+    if (playerChoice === "rock") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Player Wins!";
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Computer Wins!";
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+    //Check for paper
+    if (playerChoice === "paper") {
+      if (computerChoice === "rock") {
+        winner.textContent = "Player Wins!";
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Computer Wins!";
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+    //Check for scissors
+    if (playerChoice === "scissors") {
+      if (computerChoice === "paper") {
+        winner.textContent = "Player Wins!";
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Computer Wins!";
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+  };
+
+  const updateScore = () => {
+    const playerScore = document.querySelector(".player-score p");
+    const computerScore = document.querySelector(".computer-score p");
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
   };
 
   //Call all the inner functions here
   startGame();
+  playMatch();
 };
 
 //Call the main function (start game);
